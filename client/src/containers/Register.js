4fc7f16374f4b023ab8007d867384f2dbeada81b
WaunBroderick <waunbroderick@gmail.com>
2021-11-20 23:06:08 -0500

    Began to create the sign-up form layout

--- client/src/containers/Register.js
+++ client/src/containers/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { EuiFieldPassword, EuiFieldText, EuiSwitch } from '@elastic/eui';
+import { EuiFieldPassword,  EuiForm, EuiFormRow, EuiFieldText, EuiSpacer, EuiButton, EuiFlexGrid, EuiSwitch, EuiCard, EuiIcon, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 
 import '@elastic/eui/dist/eui_theme_light.css';
 
@@ -24,13 +24,58 @@ export default function Register(){
 
     return(
         <div>
-            <h1>Hello All</h1>
-            <EuiFieldText
-                placeholder="Placeholder text"
-                value={value}
-                onChange={(e) => onChange(e)}
-                aria-label="Use aria labels when no actual label is in use"
-            />
+            <EuiFlexGroup justifyContent="spaceAround" >
+                <EuiFlexItem grow={false}>
+                            <EuiCard
+                            icon={<EuiIcon size="s" type="dashboardApp" />}
+                            title="Register Your Organization"
+                            description="Example of a card's description. Stick to one or two sentences."
+                            betaBadgeProps={{
+                                label: 'Sign Up',
+                                tooltipContent:
+                                  'Fill out the inforamtion below and click Sign Up to create your Conduit Account.',
+                              }}
+                            onClick={() => {}}
+                            >
+                                
+                                <EuiForm>
+                                    <EuiFormRow label='Organization Name'>
+                                        <EuiFieldText name="organization"/>
+                                    </EuiFormRow>
+
+                                    <EuiFormRow label='Email Address'>
+                                        <EuiFieldText name="Email"/>
+                                    </EuiFormRow>
+
+                                    <EuiFormRow  label='Create Password'>
+                                    <EuiFieldPassword
+                                        placeholder="Placeholder text"
+                                        value={value}
+                                        onChange={onChange}
+                                        type="dual"
+                                        />
+                                    </EuiFormRow>
+
+                                    <EuiFormRow  label='Confirm Password'>
+                                    <EuiFieldPassword
+                                        placeholder="Placeholder text"
+                                        value={value}
+                                        onChange={onChange}
+                                        type="dual"
+                                        />
+                                    </EuiFormRow>
+
+                                    <EuiSpacer />
+
+                                    <EuiButton type="submit" fill>
+                                    Sign-Up
+                                    </EuiButton>
+
+                                </EuiForm>
+
+                            </EuiCard>
+                </EuiFlexItem>
+            </EuiFlexGroup>
         </div>
     )
 }

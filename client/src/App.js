3d63ec74f17ffa5bc78d4dc5361340ba210304f2
WaunBroderick <waunbroderick@gmail.com>
2022-04-22 07:46:26 -0400

    :construction_worker: CI: Fixing CI build

--- client/src/App.js
+++ client/src/App.js
@@ -15,13 +15,13 @@ import Users from "./containers/Users/Users";
 import Marketplace from "./containers/Marketplace/Marketplace";
 import Organization from "./containers/Organization/Organization";
 import Profile from "./containers/Profile/Profile";
+import Courses from "./containers/Courses/Courses";
 
 // Themeing
 import WebFont from "webfontloader";
 import { useTheme } from "./styles/themes/useTheme";
 import { GlobalStyles } from "./styles/themes/GlobalStyles";
 import { getFromLS } from "./utils/storage";
-import Courses from "./containers/Courses/Courses";
 
 // SWR Experiment
 import useSWR, { SWRConfig } from "swr";

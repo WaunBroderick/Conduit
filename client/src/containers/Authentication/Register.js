5c66afbd7a691415948ddcf92579f77958bb34aa
WaunBroderick <waunbroderick@gmail.com>
2022-09-21 15:17:30 -0400

    :lipstick: WIP: Updating the side navigation menu

--- client/src/containers/Authentication/Register.js
+++ client/src/containers/Authentication/Register.js
@@ -73,72 +73,77 @@ export default function Register() {
   }
 
   return (
-    <StyledSection>
-      <EuiFlexGroup justifyContent="spaceAround" alignItems="center">
-        <EuiFlexItem grow={false}>
-          <EuiCard
-            icon={<EuiIcon size="s" type="dashboardApp" />}
-            title="Register Your Organization"
-            description="Example of a card's description. Stick to one or two sentences."
-            betaBadgeProps={{
-              label: "Sign Up",
-              tooltipContent:
-                "Fill out the inforamtion below and click Sign Up to create your Conduit Account.",
-            }}
-            onClick={() => {}}
-          >
-            <EuiForm onSubmit={submit}>
-              <EuiFormRow label="Organization Name">
-                <EuiFieldText
-                  name="organization"
-                  onChange={(e) => setOrganization(e.target.value)}
-                />
-              </EuiFormRow>
+    <>
+      <StyledSection>
+        <EuiFlexGroup justifyContent="spaceAround" alignItems="center">
+          <EuiFlexItem grow={false}>
+            <EuiCard
+              icon={<EuiIcon size="s" type="dashboardApp" />}
+              title="Register Your Organization"
+              description="Example of a card's description. Stick to one or two sentences."
+              betaBadgeProps={{
+                label: "Sign Up",
+                tooltipContent:
+                  "Fill out the inforamtion below and click Sign Up to create your Conduit Account.",
+              }}
+              onClick={() => {}}
+            >
+              <EuiForm onSubmit={submit}>
+                <EuiFormRow label="Organization Name">
+                  <EuiFieldText
+                    name="organization"
+                    onChange={(e) => setOrganization(e.target.value)}
+                  />
+                </EuiFormRow>
 
-              <EuiFormRow label="Email Address">
-                <EuiFieldText
-                  name="Email"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </EuiFormRow>
+                <EuiFormRow label="Email Address">
+                  <EuiFieldText
+                    name="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </EuiFormRow>
 
-              <EuiFormRow label="Personal Name">
-                <EuiFieldText
-                  name="Name"
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </EuiFormRow>
+                <EuiFormRow label="Personal Name">
+                  <EuiFieldText
+                    name="Name"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </EuiFormRow>
 
-              <EuiFormRow label="Create Password">
-                <EuiFieldPassword
-                  placeholder="Placeholder text"
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="dual"
-                />
-              </EuiFormRow>
+                <EuiFormRow label="Create Password">
+                  <EuiFieldPassword
+                    placeholder="Placeholder text"
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="dual"
+                  />
+                </EuiFormRow>
 
-              <EuiFormRow label="Confirm Password">
-                <EuiFieldPassword placeholder="Placeholder text" type="dual" />
-              </EuiFormRow>
+                <EuiFormRow label="Confirm Password">
+                  <EuiFieldPassword
+                    placeholder="Placeholder text"
+                    type="dual"
+                  />
+                </EuiFormRow>
+
+                <EuiSpacer />
+
+                <EuiButton type="submit" fill onClick={(e) => submit(e)}>
+                  Sign-Up
+                </EuiButton>
+              </EuiForm>
 
               <EuiSpacer />
 
-              <EuiButton type="submit" fill onClick={(e) => submit(e)}>
-                Sign-Up
-              </EuiButton>
-            </EuiForm>
-
-            <EuiSpacer />
-
-            <EuiText>
-              <p>
-                If you already have an account please{" "}
-                <Link to="/signin">Sign-In</Link>.
-              </p>
-            </EuiText>
-          </EuiCard>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    </StyledSection>
+              <EuiText>
+                <p>
+                  If you already have an account please{" "}
+                  <Link to="/signin">Sign-In</Link>.
+                </p>
+              </EuiText>
+            </EuiCard>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </StyledSection>
+    </>
   );
 }
